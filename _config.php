24083bb7105f19a39dacd938b1cf2eefac691eef
ILY-1606 <no1.ily1606@gmail.com>
2021-06-02 19:55:33 +0700

    Update

--- _config.php
+++ _config.php
@@ -3,6 +3,7 @@ $db_server = "localhost";
 $db_username = "root";
 $db_password = "";
 $db_dbname = "lifechat";
+date_default_timezone_set("Asia/Ho_Chi_Minh");
 if(isset($_SERVER["REQUEST_SCHEME"]) && isset($_SERVER["HTTP_HOST"]))
 $serverUrl = $_SERVER["REQUEST_SCHEME"]."://".$_SERVER["HTTP_HOST"]
 ?>
