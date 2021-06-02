c975d709f56baca87bd28249f5fda275aa2f5aae
ILY-1606 <no1.ily1606@gmail.com>
2021-06-02 20:06:07 +0700

    Update for chat.php

--- _connect.php
+++ _connect.php
@@ -1,5 +1,8 @@
 <?php
+if(!empty($_SERVER["DOCUMENT_ROOT"]))
 include($_SERVER["DOCUMENT_ROOT"]."/_config.php");
+else
+include("./_config.php");
 $conn = new mysqli($db_server,$db_username,$db_password,$db_dbname);
 if($conn->connect_errno){
     die("Failed connect to Mysql:".$mysqli -> connect_error);
