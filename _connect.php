5a68d55bf49eddc44f87373a7d57ed43761dbc96
ILY-1606 <no1.ily1606@gmail.com>
2021-06-02 19:56:15 +0700

    Update

--- _connect.php
+++ _connect.php
@@ -1,5 +1,5 @@
 <?php
-include($_SERVER["DOCUMENT_ROOT"]."./_config.php");
+include($_SERVER["DOCUMENT_ROOT"]."/_config.php");
 $conn = new mysqli($db_server,$db_username,$db_password,$db_dbname);
 if($conn->connect_errno){
     die("Failed connect to Mysql:".$mysqli -> connect_error);
